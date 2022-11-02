4c3362c0794075a2a354e66262196d2cbef54bb5
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2022-11-02 18:41:32 +0000

    Added base calculator code.

--- CalculatorTests.py
+++ CalculatorTests.py
@@ -2,8 +2,13 @@ from Calculator import convertToPostfix, evalPostfix
 
 # Tests for convertToPostfix.
 def testConvertToPostfix():
-	assert convertToPostfix("5 - 10") == ""
+	assert convertToPostfix("5 - 10") == "5 10 -"
+	assert convertToPostfix("5 + 3 - 2") == "5 3 + 2 -"
+	assert convertToPostfix("10 * 2 + 3 - 6") == "10 2 * 3 + 6 -"
 
 # Tests for evalPostfix.
 def testEvalPostfix():
-	assert evalPostfix("2 4 +") == -1
+	assert evalPostfix("2 4 +") == 6
+	assert evalPostfix("3 3 *") == 9
+	assert evalPostfix("9 6 -") == 3
+	assert evalPostfix("4 3 * 12 + 4 -") == 20
