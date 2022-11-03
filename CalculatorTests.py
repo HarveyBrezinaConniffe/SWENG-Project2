0908f3fea181b3ae2eb43e159131f1cd5fafccf1
emukperv <emukperv@tcd.ie>
2022-11-03 01:26:26 +0000

    Dockerised Web App for String Calculator

--- CalculatorTests.py
+++ CalculatorTests.py
@@ -1,14 +1,16 @@
-from Calculator import convertToPostfix, evalPostfix
+from Calculator import convert_postfix, solve_postfix
 
-# Tests for convertToPostfix.
-def testConvertToPostfix():
-	assert convertToPostfix("5 - 10") == "5 10 -"
-	assert convertToPostfix("5 + 3 - 2") == "5 3 + 2 -"
-	assert convertToPostfix("10 * 2 + 3 - 6") == "10 2 * 3 + 6 -"
 
-# Tests for evalPostfix.
-def testEvalPostfix():
-	assert evalPostfix("2 4 +") == 6
-	assert evalPostfix("3 3 *") == 9
-	assert evalPostfix("9 6 -") == 3
-	assert evalPostfix("4 3 * 12 + 4 -") == 20
+# Tests for convert_postfix.
+def test_convert_postfix():
+    assert convert_postfix("5 - 10") == "5 10 -"
+    assert convert_postfix("5 + 3 - 2") == "5 3 + 2 -"
+    assert convert_postfix("10 * 2 + 3 - 6") == "10 2 * 3 + 6 -"
+
+
+# Tests for solve_postfix.
+def test_solve_postfix():
+    assert solve_postfix("2 4 +") == 6
+    assert solve_postfix("3 3 *") == 9
+    assert solve_postfix("9 6 -") == 3
+    assert solve_postfix("4 3 * 12 + 4 -") == 20
